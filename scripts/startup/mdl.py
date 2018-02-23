0d2c0b8796c2dff0cc376dd3b7728447d58a8157
Jas03x <jas03x@gmail.com>
2018-02-22 23:22:32 -0500

    Basic mesh data

--- scripts/startup/mdl.py
+++ scripts/startup/mdl.py
@@ -6,11 +6,22 @@ class MDL_Exporter(bpy.types.Operator):
     bl_label = "Export MDL"
 
     filepath = bpy.props.StringProperty(subtype="FILE_PATH")
-    
+
     def execute(self, context):
         out = open(self.filepath, 'w')
-        for object in bpy.data.objects:
-            out.write("" + str(object.name))
+
+        for obj in bpy.data.objects:
+            if obj.name in ['Lamp', 'Camera']:
+                continue
+            mesh = bpy.data.meshes[obj.name]
+            out.write(obj.name + ":\n")
+            out.write("Vertices:\n")
+            for vertex in mesh.vertices:
+                out.write(str(vertex.co[0]) + "," + str(vertex.co[1]) + "," + str(vertex.co[2]) + "\n")
+            out.write("Polygons:\n")
+            for face in mesh.polygons:
+                out.write(str(face.vertices[0]) + "," + str(face.vertices[1]) + "," + str(face.vertices[2]) + "\n")
+
         out.close()
         return {'FINISHED'}
 
