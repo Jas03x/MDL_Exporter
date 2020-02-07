5b5d397bea0e9700325f73f84976af3633c9ef25
Jas <jas03x@gmail.com>
2020-02-07 14:03:55 -0500

    fixed folder name

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 
-cp ./scripts/addons/mdl_exporter.py /Applications/Blender.app/Contents/Resources/2.81/scripts/startup/mdl_exporter.py
+cp ./scripts/startup/mdl_exporter.py /Applications/Blender.app/Contents/Resources/2.81/scripts/startup/mdl_exporter.py
