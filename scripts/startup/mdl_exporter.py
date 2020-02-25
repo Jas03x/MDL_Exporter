61ea4d1ecf9e6c1c80571ab611a45bf8597acd7c
Jas <jas03x@gmail.com>
2020-02-24 19:47:18 -0500

    Added vertex data

--- scripts/startup/mdl_exporter.py
+++ scripts/startup/mdl_exporter.py
@@ -89,10 +89,6 @@ class MDL_Exporter(bpy.types.Operator, ExportHelper):
         node_index = Index()
 
         for obj in bpy.data.objects:
-            if obj.rotation_mode != 'XYZ':
-                self.report({"ERROR"}, "Invalid rotation mode")
-                return {"CANCELLED"}
-
             p_index = -1 if obj.parent is None else node_index.get_index(obj.parent.name)
             if (obj.parent != None) and (p_index == -1):
                 self.report({"ERROR"}, "Parent not found")
@@ -128,6 +124,10 @@ class MDL_Exporter(bpy.types.Operator, ExportHelper):
         for bone in bone_index.array:
             write_matrix(f, "bone {}:".format(bone.name), bone.offset_matrix)
         
+        vertex_set = []
+        vertex_map = {}
+        index_array = []
+
         for mesh in bpy.data.meshes:
             mesh_node = node_index.get(mesh.name)
             mesh_parent = None if (mesh_node.parent == -1) else node_index.array[mesh_node.parent]
@@ -142,24 +142,18 @@ class MDL_Exporter(bpy.types.Operator, ExportHelper):
                 skeleton_offset = bpy.data.objects[armature.name].matrix_world.to_translation()
                 offset = skin_offset - skeleton_offset
                 
-                bind_shape_matrix = Matrix()
                 bind_shape_matrix[0] = [1, 0, 0, 0]
                 bind_shape_matrix[1] = [0, 0, 1, 0]
                 bind_shape_matrix[2] = [0, -1, 0, 0]
                 bind_shape_matrix[3] = [offset[0], offset[1], offset[2], 1]
             write_matrix(f, "bind shape matrix:", bind_shape_matrix)
 
-        '''
-        for mesh in bpy.data.meshes:
             uv_layer = mesh.uv_layers.active.data
 
             vertex_group_map = [] # maps the group index to the bone index
             for group in bpy.data.objects[mesh.name].vertex_groups:
-                vertex_group_map.append(bone_map[group.name])
+                vertex_group_map.append(bone_index.get_index(group.name))
 
-            vertex_list = []
-            vertex_map  = {}
-            indices = []
             for face in mesh.polygons:
                 if face.loop_total != 3:
                     self.report({"ERROR"}, "Mesh has non-triangular polygons")
@@ -169,7 +163,7 @@ class MDL_Exporter(bpy.types.Operator, ExportHelper):
                     if len(v.groups) > 4:
                         self.report({"ERROR"}, "A vertex has more than 4 bones")
                         return {"CANCELLED"}
-                    vertex = MDL_Vertex(v.co, v.normal, uv_layer[i].uv)
+                    vertex = MDL_Vertex(bind_shape_matrix @ v.co, v.normal, uv_layer[i].uv)
                     for g in v.groups:
                         vertex.bone_indices[vertex.bone_count] = vertex_group_map[g.group]
                         vertex.bone_weights[vertex.bone_count] = g.weight
@@ -178,20 +172,20 @@ class MDL_Exporter(bpy.types.Operator, ExportHelper):
 
                     index = vertex_map.get(vertex, -1)
                     if index == -1:
-                        vertex_map[vertex] = len(vertex_list)
-                        vertex_list.append(vertex)
-                    indices.append(index)
-            for vertex in vertex_list:
-                f.write("({0:.6f}, {1:.6f}, {2:.6f}), ({3:.6f}, {4:.6f}, {5:.6f}), ({6:.6f}, {7:.6f}), ({8:.6f}, {9:.6f}, {10:.6f}, {11:.6f}), ({12:.6f}, {13:.6f}, {14:.6f}, {15:.6f}), {16:.6f}\n".format(
-                    vertex.position[0], vertex.position[1], vertex.position[2],
-                    vertex.normal[0], vertex.normal[1], vertex.normal[2],
-                    vertex.uv[0], vertex.uv[1],
-                    vertex.bone_indices[0], vertex.bone_indices[1], vertex.bone_indices[2], vertex.bone_indices[3],
-                    vertex.bone_weights[0], vertex.bone_weights[1], vertex.bone_weights[2], vertex.bone_weights[3],
-                    vertex.bone_count
-                ))
-            f.write("SAVED {} WRITES\n".format(len(indices) - len(vertex_list)))
-        '''
+                        vertex_map[vertex] = len(vertex_set)
+                        vertex_set.append(vertex)
+                    index_array.append(index)
+            
+        for vertex in vertex_set:
+            f.write("({0:.6f}, {1:.6f}, {2:.6f}), ({3:.6f}, {4:.6f}, {5:.6f}), ({6:.6f}, {7:.6f}), ({8:.6f}, {9:.6f}, {10:.6f}, {11:.6f}), ({12:.6f}, {13:.6f}, {14:.6f}, {15:.6f}), {16:.6f}\n".format(
+                vertex.position[0], vertex.position[1], vertex.position[2],
+                vertex.normal[0], vertex.normal[1], vertex.normal[2],
+                vertex.uv[0], vertex.uv[1],
+                vertex.bone_indices[0], vertex.bone_indices[1], vertex.bone_indices[2], vertex.bone_indices[3],
+                vertex.bone_weights[0], vertex.bone_weights[1], vertex.bone_weights[2], vertex.bone_weights[3],
+                vertex.bone_count
+            ))
+        f.write("SAVED {} WRITES\n".format(len(index_array) - len(vertex_set)))
 
         f.close()
         
